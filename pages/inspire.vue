16b1d514b801521096999c31c796e0fd84cd8d00
Ray Bonander <ray@sendlane.com>
2018-10-31 22:08:53 -0700

    update name

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -6,7 +6,7 @@
         <h3 class="subtitle is-6 has-text-grey">
             Author:
             <a href="https://github.com/anteriovieira">
-                Antério Vieira
+                Ray Bonander
             </a>
         </h3>
     </section>
